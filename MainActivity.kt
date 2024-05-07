b84d279e4d1e062d071555b231117867150f19d4
jay <kanadejay08@gmail.com>
2024-05-07 17:01:26 +0530

    Calculator Project

---
+++ MainActivity.kt
@@ -0,0 +1,159 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    private var canAddOperation = false
+    private var canAddDecimal = true
+
+    private lateinit var workingsScreen: TextView
+    private lateinit var resultsScreen: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        workingsScreen = findViewById(R.id.workingsScreen)
+        resultsScreen = findViewById(R.id.resultsScreen)
+    }
+
+    fun numberAction(view: View) {
+        if (view is Button) {
+            val buttonText = view.text.toString()
+
+            if (buttonText == ".") {
+                if (canAddDecimal && !workingsScreen.text.contains(".")) {
+                    workingsScreen.append(buttonText)
+                    canAddDecimal = false
+                }
+            } else {
+                workingsScreen.append(buttonText)
+                canAddOperation = true
+                canAddDecimal = true
+            }
+        }
+    }
+
+
+    fun operationAction(view: View) {
+        if(view is Button && canAddOperation) {
+            workingsScreen.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
+    fun allClearAction(view: View) {
+        workingsScreen.text = ""
+        resultsScreen.text = ""
+    }
+
+    fun backSpaceAction(view: View) {
+        val length = workingsScreen.length()
+        if(length > 0)
+            workingsScreen.text = workingsScreen.text.subSequence(0, length - 1)
+    }
+
+    fun equalsAction(view: View) {
+        resultsScreen.text = calculateResults()
+    }
+
+    private fun calculateResults(): String {
+        val digitsOperators = digitsOperators()
+        if (digitsOperators.isEmpty()) return ""
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if (timesDivision.isEmpty()) return ""
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float {
+        var result = passedList[0] as Float
+        for(i in passedList.indices) {
+            if(passedList[i] is Char && i != passedList.lastIndex) {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+')
+                    result += nextDigit
+                if (operator == '-')
+                    result -= nextDigit
+            }
+        }
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
+        var list = passedList
+        while (list.contains('x') || list.contains('/') || list.contains('%')) {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun moduloCalculate(passedList: MutableList<Any>): Float {
+        var result = passedList[0] as Float
+        for (i in passedList.indices) {
+            if (passedList[i] is Char && i != passedList.lastIndex) {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '%')
+                    result %= nextDigit
+            }
+        }
+        return result
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+        for(i in passedList.indices) {
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex) {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator) {
+                    'x' -> {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' -> {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '%' -> {
+                        newList.add((prevDigit / 100) * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else -> {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any> {
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for(character in workingsScreen.text) {
+            if(character.isDigit() || character == '.')
+                currentDigit += character
+            else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+        if(currentDigit != "")
+            list.add(currentDigit.toFloat())
+        return list
+    }
+}
